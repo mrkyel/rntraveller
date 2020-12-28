78117b14e4b2b5de7f3e0bfe57f455ee7555c00c
mrkyel <kyel100@naver.com>
2020-12-28 20:01:30 +0900

    TS를 기반으로 한 개발환경 셋팅 완료

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import { Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+
+
+const App =  () => {
+  return (
+    <>
+      <View><Text>뭐라고 말좀해</Text></View>
+    </>
+  );
+};
+
+export default App;
